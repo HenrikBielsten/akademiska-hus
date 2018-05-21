b085fefa04b180c5f3c0e9883e70560373ecf681
Filip <Fpetersson96@gmail.com>
2018-05-21 11:59:16 +0200

    updated with laravelmix and sass

--- javascript/takePictureView/continue.js
+++
@@ -1,9 +0,0 @@
-const continueButton = document.querySelector('.continueButton');
-const locationText2 = document.querySelector('.locationText-2');
-
-continueButton.addEventListener("click", (e) => {
-  console.log(filesArray);
-  console.log(location);
-
-  localStorage.
-})
---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+ mix.js('src/js/app.js', 'js/app.js')
+    .sass('src/sass/app.scss', 'css/app.css');
+
+    mix.combine([
+      'src/js/takePictureView/geolocation.js',
+        'src/js/takePictureView/addFiles.js',
+        'src/js/takePictureView/urgentModal.js',
+        'src/js/takePictureView/continue.js'
+    ], 'js/app.js');
